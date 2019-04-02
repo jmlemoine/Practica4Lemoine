a861e5502ae342a49ef459fe8251a43b57718d36
jmlemoine <jeanmelvinlp27@gmail.com>
2019-04-02 17:00:12 -0400

    Arreglando detalles

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,4 +1,28 @@
 package Main;
 
+import Servicios.BaseDatosServicios;
+import Servicios.BootStrapServicios;
+import Servicios.UsuarioServicios;
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+
+            BootStrapServicios.iniciarBaseDatos();
+
+            BaseDatosServicios.getInstancia().testConexion();
+
+            BootStrapServicios.crearTablas();
+
+            UsuarioServicios serviciouser = new UsuarioServicios();
+            serviciouser.crearUsuarioPorDefecto();
+
+            Enrutamiento.crearRutas();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
